ffd63b9973976ebcb22c0d3fb4a9806ce512d4d9
gokcvkk <119782405+gokcvkk@users.noreply.github.com>
2023-10-04 10:26:16 +0300

    Add files via upload

---
+++ kelebekkk.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	
+	int i,j;
+	int kelebek[i][j];
+
+for(int i=0; i<8; i++)
+{
+    for(int j=0; j<17; j++)
+    {
+        printf("%c", kelebek[i][j]);
+    }
+    printf("\n");
+}
+
+return 0;
+}
